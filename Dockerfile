1ed702f320484a829986730a9bf04a071b1ec709
Thanthan77 <ethanqc.chea@gmail.com>
2026-01-03 11:56:16 -0500

    site devrait etre fonctionnel

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM php:8.2-cli
 WORKDIR /app
 COPY . .
 
-CMD ["php", "-S", "0.0.0.0:10000", "-t", "api"]
+CMD ["php", "-S", "0.0.0.0:10000", "-t", "site"]
+
--- api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-echo json_encode([
-    "status" => "online",
-    "message" => "Backend Canadiens API is running on Render 🚀",
-    "time" => date("Y-m-d H:i:s")
-]);
